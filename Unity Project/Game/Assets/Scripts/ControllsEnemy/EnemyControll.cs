9c737047ff5c397dc9ffd9aa3bf5bad3a3bbb683
Firemanza <markrimmerza@gmail.com>
2020-06-17 21:50:54 +0200

    enemy

---
+++ Unity Project/Game/Assets/Scripts/ControllsEnemy/EnemyControll.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyControll : MonoBehaviour
+{
+    private NavMeshAgent Mob;
+
+    public GameObject player;
+
+    public float MobDistRun = 4.0f;
+
+    private void Start()
+    {
+        Mob = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+
+        if (distance < MobDistRun)
+        {
+            Vector3 dirToPlayer = transform.position - player.transform.position;
+
+            Vector3 newPos = transform.position - dirToPlayer;
+
+            Mob.SetDestination(newPos);
+        }
+    }
+   
+}
---
+++ Unity Project/Game/Assets/Scripts/ControllsEnemy/Enemyr.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemyr : MonoBehaviour
+{
+    UnityEngine.AI.NavMeshAgent myNavMesh;
+
+    public Transform playerTransform;
+    public float checkRate = 0.01f;
+    
+    float nextCheck;
+    void Start()
+    {
+        
+            
+
+        myNavMesh = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextCheck)
+        {
+            nextCheck = Time.time + checkRate;
+            FollowPlayer();
+        }
+            
+    }
+    void FollowPlayer()
+    {
+        myNavMesh.transform.LookAt(playerTransform);
+        myNavMesh.destination = playerTransform.position;
+    }
+}
